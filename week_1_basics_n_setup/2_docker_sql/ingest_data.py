18f6ecf8cecccc8609cd61e75217cd3103797ab4
Stayermax <vitalkrat@gmail.com>
2022-01-26 07:03:16 +0300

    no comments

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -19,36 +19,36 @@ def main(params):
     table_name = params.table_name
     url = params.url
     csv_name = 'output.csv'
-
     os.system(f"wget {url} -O {csv_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
-
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
+    try:
+        while True:
+            t_start = time()
 
-    while True: 
-        t_start = time()
+            df = next(df_iter)
 
-        df = next(df_iter)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df.to_sql(name=table_name, con=engine, if_exists='append')
 
-        df.to_sql(name=table_name, con=engine, if_exists='append')
+            t_end = time()
 
-        t_end = time()
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+    except Exception:
+        print("That's all folks")
 
-        print('inserted another chunk, took %.3f second' % (t_end - t_start))
 
 
 if __name__ == '__main__':
